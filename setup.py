979a9da571e621b5a4f282fba3bb4d4215dc320d
Ramiro Wong <ramiro.wong@galile.edu>
2024-10-21 21:12:11 -0600

    Modelo base inicial

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Proyecto para predecir el churn de los clientes de un banco',
+    author='Universidad Galileo',
+    license='',
+)
